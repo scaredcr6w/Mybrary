f117e4de534ff6ad921ee898ab7fd13c399fcfd7
andalevente <andalevente2003@gmail.com>
2024-05-18 12:21:47 +0200

    Initial Commit

---
+++ Mybrary/MybraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MybraryApp.swift
+//  Mybrary
+//
+//  Created by Anda Levente on 18/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct MybraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
