eb0c7762cf7919457448fedac101cb5d6287f381
andalevente <andalevente2003@gmail.com>
2024-06-18 21:56:43 +0200

    statistics

---
+++ Mybrary/Screens/Statistics/StatisticsView.swift
@@ -0,0 +1,18 @@
+//
+//  StatisticsView.swift
+//  Mybrary
+//
+//  Created by Anda Levente on 18/06/2024.
+//
+
+import SwiftUI
+
+struct StatisticsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StatisticsView()
+}
---
+++ Mybrary/Screens/Statistics/StatisticsViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  StatisticsViewModel.swift
+//  Mybrary
+//
+//  Created by Anda Levente on 18/06/2024.
+//
+
+import Foundation
